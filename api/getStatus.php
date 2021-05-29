01fd8e1e80a14e5ac5cbdabe19f414c9f3c89c9a
George Rafailides <clean.code.tech@gmail.com>
2021-05-29 11:11:30 +0300

    bugfix 1.4

--- api/getStatus.php
+++ api/getStatus.php
@@ -17,9 +17,9 @@ for ( $i = 0 ; $i < count( $_POST ) ; $i++ ) {
 
     } else {
 
-        $curlOutput = shell_exec( $output[ 0 ] . ' -s -o /dev/null -w "%{http_code}" -L -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US,en;q=0.5" -H "Cache-Control: max-age=0" -H "Connection: keep-alive" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0" ' . $_POST[ $i ] );
+        $curlOutput = (int) shell_exec( $output[ 0 ] . ' -s -o /dev/null -w "%{http_code}" -L -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US,en;q=0.5" -H "Cache-Control: max-age=0" -H "Connection: keep-alive" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0" ' . $_POST[ $i ] );
 
-        if ( $curlOutput === '200' ) {
+        if ( $curlOutput >= 200 && $curlOutput < 500 ) {
 
             $response[ $_POST[ $i ] ] = 1;
 
